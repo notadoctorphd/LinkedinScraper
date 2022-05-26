c16f9bf1fd4610373c867b6631eb9b66bb8e51c3
notadoctorphd <evan.lanoil@protonmail.com>
2022-05-25 22:15:24 -0600

    Work around for login and basic data extraction

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import censusgeocode as cg
 import time
 
 class Scraper:
 
-    global username, password
+    global username, password, PATH, driver  
     username = input("Type Username: ")
-    password = input("Type Password: ")
-
+    password = input("Type Password: ")              
+    PATH = "/usr/bin/chromedriver"
+    driver = webdriver.Chrome(PATH)
+    
     def __init__(self):
         pass          
 
@@ -19,10 +22,6 @@ class Scraper:
 
         self.username = username
         self.password = password
-
-               
-        PATH = "/usr/bin/chromedriver"
-        driver = webdriver.Chrome(PATH)
 
         driver.get('https://www.linkedin.com')
         print(driver.title)
@@ -35,7 +34,6 @@ class Scraper:
         #find elm passowrd and send password
         passwrd = driver.find_element_by_id("session_password")
         passwrd.send_keys(password)
-        passwrd.send_keys(Keys.RETURN)
 
         time.sleep(5)
 
@@ -43,26 +41,42 @@ class Scraper:
         signin.click()
 
     
-     
-        # username = secrets.username  #this is where your username
-        # password = secrets.password
-    def web_scrape():
+      
+    global search_job, search_location #should set global variable for search_job and should i
+    search_job = input("Search by title, skill, or company: ")
+    search_location = input("Enter location: ")
+    # search_location = search_location.replace(", ", "%2C%20")
+    # search_location = cg.onelineaddress(search_location)
+    # location=Denver%2C%20Colorado%2C%20United%20States
 
-        pass
-       
+    def job_scrapes(self):
+        # https://www.linkedin.com/jobs/search/?geoId=103644278&keywords=Recruit&location=United%20States
+        site_linkedin = str(("https://www.linkedin.com/jobs/search/?geoId=103644278&keywords=" + search_job))
+        driver.get(site_linkedin)
+        print(driver.title)
+        time.sleep(3)
+        # job_search = driver.find_element_by_id("jobs-search-box-keyword-id-ember30")
+        # job_search.double_click()
+        # time.sleep(3)
+        # Scraper.cleartext(job_search)
+        # job_search.send_keys(search_job)
+        # 
 
+        job_location = driver.find_element_by_id("jobs-search-box-location-id-ember30")
+        job_location.click()
+        time.sleep(3)
+        job_location.clear()
+        time.sleep(3)
+        job_location.send_keys(search_location)
 
-
-# print (Scraper.login(Scraper, username, password))
-
+        button_search = driver.find_element_by_xpath("/html/body/div[5]/header/div/div/div/div[2]/button[1]")
+        button_search.click()
+       # else:
+            # job_location.send_keys(search_job)
+            # 
 Scraper.login(Scraper, username, password)
-# 
-# try:
-    # main = WebDriverWait(driver, 10).until(
-        # EC.presence_of_element_located((By.ID, "main"))
-    # )
-# 
-# except:
-    # driver.quit()
+Scraper.job_scrapes(Scraper)
+
+
 # 
 # print(main.text)
